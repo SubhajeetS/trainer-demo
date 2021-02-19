d54ca9aa95dcf7851720025d24bbaa4e2040838b
sjsorc <subhajeet17sarkar@gmail.com>
2021-02-19 11:28:30 -0800

    server configuration

--- src/client/config.js
+++ src/client/config.js
@@ -1,3 +1,3 @@
 export default {
-    SERVER_BASE_URL: "http://ec2-18-191-151-130.us-east-2.compute.amazonaws.com:8100/v1"
+    SERVER_BASE_URL: "/v1"
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const os = require('os');
+const proxy = require('express-http-proxy');
 
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.use('/', proxy('http://ec2-18-191-151-130.us-east-2.compute.amazonaws.com:8100'));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
